27570fe8d1ea793b1d2a4415ede69a616c6e881b
sumingcheng <cjwdyzdyplxjg@gmail.com>
2023-10-18 14:29:34 +0800

    feat: note

--- FastAPI-Demo/start.sh
+++ FastAPI-Demo/start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# 运行 Uvicorn 服务器以启动 ASGI web 应用程序
+# 模块路径: app.main
+# --reload: 在开发模式下运行，自动重新加载服务器当源代码更改时
+# --lifespan off: 禁用生命周期事件处理
+# :app 指的是 FastAPI 应用实例的名称，即在你的代码中创建的 app = FastAPI()
+
 uvicorn app.main:app --reload --lifespan off
 
