e6ee3c7f2933f529e5ecb82bf58060bd2c7e1624
fanxiaohao <fanxiaohaohaha@qq.com>
2024-10-12 16:34:23 +0800

    fix: 下载模型

---
+++ models/download_modelscope.py
@@ -0,0 +1,10 @@
+from modelscope.hub.snapshot_download import snapshot_download
+
+# 指定模型的标识符和目标下载目录
+model_id = 'AI-ModelScope/bge-large-zh-v1.5'
+target_dir = '/home/smc/vchatbi-plugin-zz/models/bge-large-zh-v1.5'
+
+# 使用 'snapshot_download' 函数，并设置 'cache_dir' 参数
+model_dir = snapshot_download(model_id, cache_dir=target_dir)
+
+print(f"模型已下载到: {model_dir}")
---
+++ models/readme.md
@@ -0,0 +1,9 @@
+## Hugging Face 下载
+```bahs
+pip install huggingface_hub
+```
+
+## ModelScope 下载
+```bahs
+pip install modelscope
+```
